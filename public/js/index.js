b0999e5024a94d016dc6f20686e7b3c898dbbb62
Seong Han <hans@marc-hi.ca>
2018-01-24 12:22:38 -0500

    Add CSS for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -30,11 +30,12 @@ socket.on('newLocationMessage', function(message){
 jQuery('#message-form').on('submit', function (event) {
     event.preventDefault();
 
+    var messageTextbox = jQuery('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-        
+        messageTextbox.val('');
     });
 });
 
@@ -43,12 +44,17 @@ locationButton.on('click', function (event) {
     if(!navigator.geolocation){
         return alert('Geolocation not supported by yout browser');
     }
+
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) { //Success
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () { //Error 
+        locationButton.removeAttr('disabled').text('Send Location');
         alert('Unable to fetch location.');
     });
 });
