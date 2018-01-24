823a90f45da13e5c8a3512b23d3d6dbbbef61d05
Seong Han <hans@marc-hi.ca>
2018-01-24 10:32:15 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    // Emit Event from client to server
-    socket.emit('createMessage', {
-        from: 'client',
-        text: 'Hello'
-    });
 });
 
 socket.on('disconnect', function () {
