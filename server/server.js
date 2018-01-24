61357a86113060b71770ea42668e33e2a8dcf7eb
Seong Han <hans@marc-hi.ca>
2018-01-24 15:35:31 -0500

    Add Users class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,15 +14,33 @@ const publicPath = path.join(__dirname, '../public');
 
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    // socket.emit from Admin text Welcome to the chat app
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat app'));
+    socket.on('join', (params, callback) => {
+        if (!isRealString(params.name) || !isRealString(params.room)){
+            return callback('Name and room name are required!');
+        }
+        socket.join(params.room);
 
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
 
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+
+        // socket.emit from Admin text Welcome to the chat app
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat app'));
+
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+
+        callback();
+    });
+
+    socket.on('updateUserList', function (users) {
+        console.log('User list', users);
+    });
 
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
@@ -33,7 +53,12 @@ io.on('connection', (socket) => {
     })
 
     socket.on('disconnect', () => {
-        console.log('User was disconnected');
+        var user = users.removeUser(socket.id);
+        if(user){
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`));
+        }
+        
     });
 });
 app.use(express.static(publicPath));
